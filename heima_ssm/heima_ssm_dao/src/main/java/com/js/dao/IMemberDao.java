4b70fdd3d354ce57b1aa211192dbb20a7353910c
james <james@nba>
2020-02-21 11:03:35 +0800

    删除了一些

--- heima_ssm/heima_ssm_dao/src/main/java/com/js/dao/IMemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.js.dao;
-
-import com.js.domain.Member;
-import org.apache.ibatis.annotations.Select;
-//会员
-public interface IMemberDao {
-	@Select("select * from member where id=#{id}")
-	public Member findById(String id);
-}
